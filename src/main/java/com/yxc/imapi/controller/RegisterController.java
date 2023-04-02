efece32d6c395f04332c0df077ae6bc43b9490e9
yangxch <xch_yang@126.com>
2023-04-02 17:28:24 +0800

    upate im-api.

--- src/main/java/com/yxc/imapi/controller/RegisterController.java
+++ src/main/java/com/yxc/imapi/controller/RegisterController.java
@@ -71,7 +71,7 @@ public class RegisterController extends BaseNController {
         users.setUserPhone("");
         users.setEmail("");
         users.setNickName(nick_name);
-        users.setHeadUrl("https://www.lgstatic.com/i/image2/M01/3C/FE/CgoB5lzvhkyAGQbfAAHge6HweCk703.jpg");
+        users.setHeadUrl("http://124.222.4.40:8091/files/default_avatar.jpeg");
         users.setSex(1);
         users.setState(1);
         users.setCreateTime(new Date());
